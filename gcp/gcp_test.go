4841b0e202b51a5ddd0c9053d60ee030f1a1cce2
Paul White <white@tamu.edu>
2026-09-17 02:14:57 -0500

    Add unit test for listProjectsRequest

---
+++ gcp/gcp_test.go
@@ -0,0 +1,21 @@
+package gcp
+
+import "testing"
+
+func TestListProjectsRequestUsesOrganizationParent(t *testing.T) {
+	req, err := listProjectsRequest("874260368814")
+	if err != nil {
+		t.Fatalf("listProjectsRequest returned error: %v", err)
+	}
+
+	if got, want := req.GetParent(), "organizations/874260368814"; got != want {
+		t.Fatalf("parent = %q, want %q", got, want)
+	}
+}
+
+func TestListProjectsRequestRequiresOrganizationID(t *testing.T) {
+	_, err := listProjectsRequest(" ")
+	if err == nil {
+		t.Fatal("listProjectsRequest returned nil error for empty organization ID")
+	}
+}
